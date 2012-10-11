cc820dbf319f61645c3f8a21a8807ba2c22bafe6
Haeroe <lindert@cs.helsinki.fi>
2012-10-11 19:58:48 +0300

    Added help functions/parameter amount detection/ability to specify folder.

--- newrepo.sh
+++ newrepo.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+#author Haeroe
+
+if [ $1 = --help ]; then
+	echo "Example usage: newrepo.sh name reponame directory";
+	exit 0;
+fi
+
+if [ $# != 3 ]; then
+	echo "Bad arguments. Help: newrepo.sh --help";
+	exit 0;
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p $3
+cd $3
+
 echo "GitHub password for $1:"
 read -s PASS
 curl -u "$1:$PASS" https://api.github.com/user/repos -d "{\"name\":\"$2\"}"
@@ -12,3 +29,5 @@ touch .gitignore
 git add *
 git commit -m "Initial commit"
 git push origin master
+
+cd $DIR
