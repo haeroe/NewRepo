fcd7c82f8737960f74910b92aa999d80513d9940
Haeroe <lindert@cs.helsinki.fi>
2012-10-12 02:02:58 +0300

    Added quotation marks and some if statements.

--- newrepo.sh
+++ newrepo.sh
@@ -6,23 +6,36 @@ if [ $1 = --help ]; then
 	exit 0;
 fi
 
-if [ $# != 3 ]; then
+if [ $# -lt 2 ]; then
 	echo "Bad arguments. Help: newrepo.sh --help";
 	exit 0;
 fi
 
-USER=$1
-NAME=$2
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p $3
-pushd $3
+if [ $# -gt 3 ]; then
+        echo "Bad arguments. Help: newrepo.sh --help";
+        exit 0;
+fi
+
+USER="$1"
+NAME="$2"
+
+if [ $# = 3 ]; then
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+else
+	DIR="."
+fi
+
+if [ $DIR != "." ]; then
+	mkdir -p "$DIR";
+	pushd "$DIR";
+fi
 
 echo "GitHub password for ${USER}:"
 read -s PASS
 curl -u "${USER}:${PASS}" https://api.github.com/user/repos -d "{\"name\":\"${NAME}\"}"
 
 git init
-git remote add origin git@github.com:${USER}/${NAME}.git
+git remote add origin git@github.com:"${USER}"/"${NAME}".git
 
 touch README
 touch LICENSE
@@ -32,4 +45,6 @@ git add *
 git commit -m "Initial commit"
 git push origin master
 
-popd
+if [ $DIR != "." ]; then
+	popd;
+fi
