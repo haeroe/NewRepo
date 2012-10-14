320ca10b8d65299e42a20eb6ef2ab0595dde90ed
Haeroe <lindert@cs.helsinki.fi>
2012-10-14 06:38:10 +0300

    Made script silent.

--- newrepo.sh
+++ newrepo.sh
@@ -22,28 +22,27 @@ if [ $# -eq 3 ]; then
 	DIR="$3";
 fi
 
-mkdir -p "$DIR"
-pushd "$DIR"
-
+mkdir -p "$DIR" &> /dev/null
+pushd "$DIR" &> /dev/null
 
 echo "GitHub password for ${USER}:"
 read -s PASS
 
-curl -fsu "${USER}:${PASS}" https://api.github.com/user/repos -d "{\"name\":\"${NAME}\"}" || exit 1
+curl -fsu "${USER}:${PASS}" https://api.github.com/user/repos -d "{\"name\":\"${NAME}\"}" > /dev/null || exit 1
 
 if [ $# -eq 2 ]; then
 	exit 0;
 fi
 
-git init
-git remote add origin git@github.com:"${USER}"/"${NAME}".git
+git init > /dev/null
+git remote add origin git@github.com:"${USER}"/"${NAME}".git > /dev/null
 
 touch README
 touch LICENSE
 touch .gitignore
 
-git add .
-git commit -m "Initial commit"
-git push origin master
+git add . > /dev/null
+git commit -m "Initial commit" > /dev/null
+git push -q origin master > /dev/null
 
 popd
