cd7c944d3d1dee29ef27a6cfa9fb0bf88135f3d2
Haeroe <lindert@cs.helsinki.fi>
2012-10-14 05:50:24 +0300

    Initial commit

--- newrepo.sh
+++ newrepo.sh
@@ -3,36 +3,25 @@
 
 if [ $1 = --help ]; then
 	echo "Example usage: newrepo.sh name reponame directory";
-	exit 0;
+	exit 1;
 fi
 
-if [ $# -lt 2 ]; then
+if [[ $# -lt 2 || $# -gt 3 ]]; then
 	echo "Bad arguments. Help: newrepo.sh --help";
-	exit 0;
-fi
-
-if [ $# -gt 3 ]; then
-        echo "Bad arguments. Help: newrepo.sh --help";
-        exit 0;
+	exit 1;
 fi
 
 USER="$1"
 NAME="$2"
 
-if [ $# = 3 ]; then
-	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-else
-	DIR="."
-fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ $DIR != "." ]; then
-	mkdir -p "$DIR";
-	pushd "$DIR";
-fi
+mkdir -p "$DIR"
+pushd "$DIR"
 
 echo "GitHub password for ${USER}:"
 read -s PASS
-curl -u "${USER}:${PASS}" https://api.github.com/user/repos -d "{\"name\":\"${NAME}\"}"
+curl -u "${USER}:${PASS}" https://api.github.com/user/repos -d "{\"name\":\"${NAME}\"}" || exit 1
 
 git init
 git remote add origin git@github.com:"${USER}"/"${NAME}".git
@@ -41,10 +30,8 @@ touch README
 touch LICENSE
 touch .gitignore
 
-git add *
+git add .
 git commit -m "Initial commit"
 git push origin master
 
-if [ $DIR != "." ]; then
-	popd;
-fi
+popd
